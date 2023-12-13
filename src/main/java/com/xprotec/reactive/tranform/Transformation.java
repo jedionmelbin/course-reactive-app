f436b80642e2a34decf815dd6f0532b100a30840
Jedion Melbin <jedionmelbin@gmail.com>
2023-12-12 19:38:39 -0500

    [fix] add tranformation operator

--- src/main/java/com/xprotec/reactive/tranform/Transformation.java
+++ src/main/java/com/xprotec/reactive/tranform/Transformation.java
@@ -11,50 +11,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Transformation {
-    private static  final Logger log = LoggerFactory.getLogger(Transformation.class);
+    private static final Logger log = LoggerFactory.getLogger(Transformation.class);
 
-    public void map(){
+    public void map() {
         List<Person> personList = new ArrayList<>();
-        personList.add(new Person(1,"Jedion","Paucar",30));
-        personList.add(new Person(2,"Maria","Lucia",60));
-        personList.add(new Person(3,"Jedion","Paucar",40));
+        personList.add(new Person(1, "Jedion", "Paucar", 30));
+        personList.add(new Person(2, "Maria", "Lucia", 60));
+        personList.add(new Person(3, "Jedion", "Paucar", 40));
 
         Flux.fromIterable(personList)
-                .map(p-> {
+                .map(p -> {
                     p.setAge(p.getAge() + 10);
                     return p;
-                }).subscribe(c->log.info(c.toString()));
+                }).subscribe(c -> log.info(c.toString()));
 
-      Flux<Integer>  flux = Flux.range(0,10);
-      Flux<Integer> flx =  flux.map(c-> c + 10);
-      flx.subscribe(c->log.info(c.toString()));
+        Flux<Integer> flux = Flux.range(0, 10);
+        Flux<Integer> flx = flux.map(c -> c + 10);
+        flx.subscribe(c -> log.info(c.toString()));
 
     }
 
-    public void  flatMap(){
+    public void flatMap() {
         List<Person> personList = new ArrayList<>();
-        personList.add(new Person(1,"Jedion","Paucar",30));
-        personList.add(new Person(2,"Maria","Lucia",60));
-        personList.add(new Person(3,"Jedion","Paucar",40));
+        personList.add(new Person(1, "Jedion", "Paucar", 30));
+        personList.add(new Person(2, "Maria", "Lucia", 60));
+        personList.add(new Person(3, "Jedion", "Paucar", 40));
 
         //Otro flujo de datos
         Flux.fromIterable(personList)
-                .flatMap(c->{
+                .flatMap(c -> {
                     c.setAge(c.getAge() + 10);
                     return Mono.just(c);
-        }).subscribe(c->log.info(c.toString()));
+                }).subscribe(c -> log.info(c.toString()));
     }
 
-    public void groupBy(){
+    public void groupBy() {
         List<Person> personList = new ArrayList<>();
-        personList.add(new Person(1,"Jedion","Paucar",30));
-        personList.add(new Person(1,"Maria","Lucia",60));
-        personList.add(new Person(3,"Jedion","Paucar",40));
+        personList.add(new Person(1, "Jedion", "Paucar", 30));
+        personList.add(new Person(1, "Maria", "Lucia", 60));
+        personList.add(new Person(3, "Jedion", "Paucar", 40));
 
         Flux.fromIterable(personList)
                 .groupBy(Person::getId)
                 .flatMap(Flux::collectList)
-                .subscribe(c-> log.info(c.toString()));
+                .subscribe(c -> log.info(c.toString()));
+
+    }
+
+
+    public void zipOperation() {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person(1, "Jedion", "Paucar", 30));
+        personList.add(new Person(1, "Maria", "Lucia", 60));
+        personList.add(new Person(3, "Jedion", "Paucar", 40));
+
+        Flux.fromIterable(personList)
+                .groupBy(Person::getId)
+                .flatMap(Flux::collectList)
+                .subscribe(c -> log.info(c.toString()));
 
     }
 }
